92b6fe0e5dfe08f27e440645b73fce0d78853b80
Denver McCarthy <dmichaelspdx@gmail.com>
2022-04-26 14:07:37 -0700

    card info in component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export default function Card() {
-  return <div>Card</div>;
+export default function Card({
+  pokemon: { type_1, type_2, url_image, pokemon, hp },
+}) {
+  return (
+    <div className="card-container">
+      <h4>{pokemon.toUpperCase()}</h4>
+      <img src={url_image} alt={`An Image of ${pokemon}`} />
+      <p aria-label="pokemon type">
+        Type:{' '}
+        {`${type_1.toUpperCase()}${
+          type_2 === 'NA' ? '' : ` / ${type_2.toUpperCase()}`
+        }`}
+      </p>
+      <p>Hp: {hp}</p>
+    </div>
+  );
 }
--- src/views/PokeView/PokeView.jsx
+++ src/views/PokeView/PokeView.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Card from '../../components/Card/Card';
+import { usePokemonContext } from '../../context/PokemonContext';
 
 export default function PokeView() {
-  return <div>PokeView</div>;
+  const { pokemon, loading } = usePokemonContext();
+  if (loading) return <div>loading..</div>;
+  return (
+    <div>
+      <Card pokemon={pokemon[0]} />
+    </div>
+  );
 }
